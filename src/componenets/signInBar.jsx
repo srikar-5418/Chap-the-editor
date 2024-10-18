d1848914f77635e2f0f909d2f75b9a03108efd6c
srikar <srikargujja5418@gmail.com>
2024-10-18 18:25:23 +0530

    updated logo final this is working

--- src/componenets/signInBar.jsx
+++ src/componenets/signInBar.jsx
@@ -1,6 +1,6 @@
-import {Box,Flex,Button,Text} from '@chakra-ui/react'
+import {Box,Flex} from '@chakra-ui/react'
 import SignInButton from './signIn'
-
+import chapLogo from '/src/assets/chap_logo_70px.png';
 
 export default function SignInBar(){
     return (
@@ -8,7 +8,7 @@ export default function SignInBar(){
         <Box w='100%' p='1'  mt='-4' h='80px' mb='2' border='1px' borderRadius='10'>
                 <Flex justifyContent="space-between" alignItems="center">
                 <Flex mr="auto" ml="0.5%"   justifyContent='center' alignItems="center">
-                <img src="/src/assets/chap_logo_70px.png" ></img>
+                <img src={chapLogo} ></img>
                     </Flex>
                     {/* <SignInButton/>       */}
                     <Flex ml="auto" mr="0.5%"   justifyContent='center' alignItems="center">
