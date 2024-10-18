0f121bfdc4ee01b3b55b9298c3e48aafccb7b530
srikar <srikargujja5418@gmail.com>
2024-10-18 18:13:01 +0530

    updated logo 3

--- src/componenets/signInBar.jsx
+++ src/componenets/signInBar.jsx
@@ -8,7 +8,7 @@ export default function SignInBar(){
         <Box w='100%' p='1'  mt='-4' h='80px' mb='2' border='1px' borderRadius='10'>
                 <Flex justifyContent="space-between" alignItems="center">
                 <Flex mr="auto" ml="0.5%"   justifyContent='center' alignItems="center">
-                <img src="./src/assets/chap_logo_70px.png" ></img>
+                <img src="https://github.com/srikar-5418/Chap-the-editor/blob/main/src/assets/chap_logo_70px.png" ></img>
                     </Flex>
                     {/* <SignInButton/>       */}
                     <Flex ml="auto" mr="0.5%"   justifyContent='center' alignItems="center">
